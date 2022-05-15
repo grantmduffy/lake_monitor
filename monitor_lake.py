af3f488b7703de61631de8a6d498d03662d5b197
grant <grantmduffy@gmail.com>
2022-05-15 10:40:43 -0700

    Schedule captures

--- monitor_lake.py
+++ monitor_lake.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from time import sleep
 import json
 from tqdm import tqdm
+import schedule
+
 
 with open('private.txt', 'r') as f:
     api_key, rtsp_url, data_path = (x.strip() for x in f.readlines())
@@ -28,9 +30,15 @@ def capture(
     img.save(data_path / (datetime_str + '.png'))
     with open(data_path / (datetime_str + '.json'), 'w') as f:
         json.dump(r.text, f)
+    print('Captured', datetime_str)
 
 
 if __name__ == '__main__':
-    for i in tqdm(range(10)):
-        capture()
-        sleep(3)
+    # Every day 6am to 9pm in 10 min increments
+    for hour in range(6, 21):
+        for minute in range(0, 60, 10):
+            schedule.every().day.at(f'{hour:02d}:{minute:02d}').do(capture)
+    while True:
+        schedule.run_pending()
+        sleep(1)
+
